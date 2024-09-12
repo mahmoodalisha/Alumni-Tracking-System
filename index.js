df06aca1b5c9c0dfe0a0c359fe9b88aa2d95191b
Alisha <mahmoodalisha35@gmail.com>
2024-09-12 21:44:34 +0530

    changes done

--- index.js
+++ index.js
@@ -41,5 +41,7 @@ app.get('*', (req, res) => {
     )
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => {
+    //connect();
+    console.log('Server is running on port 8000');
+});
