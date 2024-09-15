1d38a16ea4ef65e387256cdf22e9489df879d57f
Alisha <mahmoodalisha35@gmail.com>
2024-09-15 20:16:44 +0530

    production added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL ;
- 
-
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const createAlumni = (token, alumniData) => {
     return axios.post(`${API_URL}/alumni`, alumniData, {
