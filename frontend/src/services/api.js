f8addcf8a6d55d886af6ee9749875dc8c64f65c0
Alisha <mahmoodalisha35@gmail.com>
2024-09-15 14:18:29 +0530

    env file added to frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/admin'; 
+const API_URL = process.env.REACT_APP_API_URL; 
 
 
 export const createAlumni = (token, alumniData) => {
--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const adminRoutes = require('./routes/adminRoute');
 const app = express();
 const db = process.env.MONGO_URI;
 
-// Connect to MongoDB
+
+app.use(express.json());
+app.use(cors());
+
 mongoose.connect(db)
     .then(() => console.log('MongoDB connected...'))
     .catch(err => {
@@ -17,43 +20,18 @@ mongoose.connect(db)
         console.error('Error message:', err.message);
     });
 
-// Serve uploaded files
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// CORS configuration: Allow only specific origins (your frontend URL)
-const allowedOrigins = [
-    'https://alumni-tracking-system-psi.vercel.app/', // Replace with your actual frontend URL
-    'http://localhost:3000' // Add localhost for development purposes
-];
-
-app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (such as mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy does not allow access from the specified origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true // Allows cookies and authorization headers to be sent
-}));
-
-// Body parser
-app.use(express.json());
-
-// Serve static frontend files (React app)
 app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
 
-// Test route
 app.get("/test", (req, res) => {
     res.send("Express app is running");
 });
 
-// API routes
 app.use('/api/admin', adminRoutes);
 
-// Catch-all route to serve frontend (React)
+
 app.get('*', (req, res) => {
     res.sendFile(
         path.resolve(__dirname, 'frontend', 'build', 'index.html'),
@@ -65,7 +43,7 @@ app.get('*', (req, res) => {
     );
 });
 
-// Start server
+
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 });
