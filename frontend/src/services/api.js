d4e402528b5ad3637761bf7d9790e56b735bf43f
Alisha <mahmoodalisha35@gmail.com>
2024-09-16 13:14:28 +0530

    backend connection changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
-
 export const createAlumni = (token, alumniData) => {
-    return axios.post(`${API_URL}/alumni`, alumniData, {
+    return axios.post(`/api/admin/alumni`, alumniData, {
         headers: {
             Authorization: `Bearer ${token}`,
             'Content-Type': 'multipart/form-data'
@@ -13,7 +11,7 @@ export const createAlumni = (token, alumniData) => {
 
 
 export const updateAlumni = (token, id, alumniData) => {
-    return axios.put(`${API_URL}/alumni/${id}`, alumniData, {
+    return axios.put(`/api/admin/alumni/${id}`, alumniData, {
         headers: {
             Authorization: `Bearer ${token}`,
             'Content-Type': 'multipart/form-data'
@@ -25,12 +23,12 @@ export const updateAlumni = (token, id, alumniData) => {
     })
     .catch(error => {
         console.error('Update error:', error.response ? error.response.data : error.message);
-        throw error; // Re-throw the error for further handling
+        throw error;
     });
 };
-// i am fetchin all alumni list
+
 export const fetchAlumni = () => {
-    return axios.get(`${API_URL}/alumni`)
+    return axios.get(`/api/admin/alumni`)
     .then(response => {
         return response.data;
     })
@@ -40,10 +38,8 @@ export const fetchAlumni = () => {
     });
 };
 
-
-// i want to delete an alumni
 export const deleteAlumni = (token, id) => {
-    return axios.delete(`${API_URL}/alumni/${id}`, {
+    return axios.delete(`/api/admin/alumni/${id}`, {
         headers: { Authorization: `Bearer ${token}` }
     });
 };
