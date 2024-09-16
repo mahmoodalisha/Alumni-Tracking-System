d62509953ec0f0289496d49761464878ebbce28e
Alisha <mahmoodalisha35@gmail.com>
2024-09-16 18:20:38 +0530

    db connected before listening

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 require("dotenv").config();
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors'); 
+const cors = require('cors');
 const path = require('path');
 const db = process.env.MONGO_URI;
 const adminRoutes = require('./routes/adminRoute');
+
+const app = express();
+
 
 app.use(express.json());
 app.use(cors({ origin: '*' }));
 
 
 mongoose.connect(db)
-    .then(() => console.log('MongoDB connected...'))
+    .then(() => {
+        console.log('MongoDB connected...');
+        
+        // Start the server only after successful connection
+        app.listen(5000, () => {
+            console.log('Server is running on port 5000');
+        });
+    })
     .catch(err => {
         console.error('MongoDB connection error:', err);
         console.error('Error code:', err.code);
         console.error('Error message:', err.message);
     });
 
+
 app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
+
 
 app.get("/test", (req, res) => {
     res.send("Express app is running");
 });
+
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+
 app.use('/api/admin', adminRoutes);
+
 
 app.get('*', (req, res) => {
     res.sendFile(
@@ -37,9 +52,4 @@ app.get('*', (req, res) => {
             }
         }
     );
-});
-
-
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
 });
